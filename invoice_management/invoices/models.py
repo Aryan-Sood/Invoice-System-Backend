6ee7de79b26756be4b4a74fe8c93cb63e5cbda78
Aryan Sood <aryansood12@gmail.com>
2024-11-25 20:20:10 +0530

    models creation

--- invoice_management/invoices/models.py
+++ invoice_management/invoices/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Invoice(models.Model):
+    id = models.AutoField(primary_key=True)
+    invoice_number = models.CharField(max_length=20, unique=True)
+    customer_name = models.CharField(max_length=30)
+    date = models.DateTimeField()
+
+class InvoiceDetail(models.Model):
+    id = models.AutoField(primary_key=True)
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    @property
+    def line_total(self):
+        return (self.unit_price * self.quantity)
