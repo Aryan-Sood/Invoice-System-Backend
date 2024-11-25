cfee4fd9ca147ea66a9961d3f96e496a002efba9
Aryan Sood <aryansood12@gmail.com>
2024-11-26 00:21:21 +0530

    debug settings

--- invoice_management/invoice_management/settings.py
+++ invoice_management/invoice_management/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=qy88+=5m=qgf39d6x&7f(f*^&_$1&ib5bjr29arur3w3j@ddh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
