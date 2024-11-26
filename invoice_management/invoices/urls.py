9b2e217fac11e4f6174ebc82af84bbc4ef6f74ea
Aryan Sood <aryansood12@gmail.com>
2024-11-26 08:46:08 +0530

    Create Invoice endpoint

--- invoice_management/invoices/urls.py
+++ invoice_management/invoices/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import InvoiceCreateView,InvoiceListView, InvoiceDetailView
 
 urlpatterns = [
-    # path("", InvoiceCreateView.as_view(), name='invoice-create')
+    path("/create", InvoiceCreateView.as_view(), name='invoice-create'),
     path("", InvoiceListView.as_view(), name='invoice-list'),
     path("/detail", InvoiceDetailView.as_view(), name='invoice-detail')
 ]
